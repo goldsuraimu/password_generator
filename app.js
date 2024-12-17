f4e4dcea3dcb3f9e9a05e23e42c8e57fc1039856
goldsuraimu <dq0938438760@gmail.com>
2024-12-17 14:56:21 +0800

    feat: add random-password.js & show password on page

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
+const passwordGenerator = require(path.join(__dirname, 'public/javasripts/random-password.js'))
 const { title } = require('process')
 
 const port = 3000
+const pwGentor = new passwordGenerator(); 
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req,res) => {
@@ -21,6 +24,28 @@ app.get('/pwgen', (req, res) => {
   res.render('index', {title: 'password generator'})
 })
 
+app.post('/pwgen', (req, res) => {
+  res.render('index', { ...dataProcesssing(req.body), title: 'password generator' })
+})
+
 app.listen(port, () => {
   console.log(`this is running at http://localhost:${port}`)
 })
+
+
+// function
+
+function dataProcesssing(body) {
+  const data = {
+    pwLength: body['pw-length'] ?? '4',
+    pwInLow: body['pw-in-low'] ?? '',
+    pwInUpper: body['pw-in-upper'] ?? '',
+    pwInNum: body['pw-in-num'] ?? '',
+    pwInSym: body['pw-in-sym'] ?? '',
+    pwExcludeChar: body['pw-exclude-char'] ?? ''
+  }
+
+  data.pwResult = pwGentor.passwordGenerator(data);
+  console.log(data)
+  return data;
+}
---
+++ public/javasripts/random-password.js
@@ -0,0 +1,42 @@
+class passwordGenerator {
+  #upperAlphabet = [];
+  #lowerAlphabet = [];
+  #numbers = [];
+  #symbols = [];
+  
+  constructor() {
+    const aCodeAt = 'a'.charCodeAt(0);
+    for(let i = 0; i < 26; i++) {
+      const alphabet = String.fromCharCode(aCodeAt + i);
+      this.#lowerAlphabet.push(alphabet);
+      this.#upperAlphabet.push(alphabet.toUpperCase());
+    }
+
+    for(let i = 0; i < 10; i++) {
+      this.#numbers.push(i.toString());
+    }
+
+    "!@#$%^&*()-+=".split('').forEach(symbol => this.#symbols.push(symbol));
+  }
+
+  passwordGenerator(data) {
+    let chars = [];
+
+    if (data.pwInLow) chars.push(...this.#lowerAlphabet);
+    if (data.pwInUpper) chars.push(...this.#upperAlphabet);
+    if (data.pwInNum) chars.push(...this.#numbers);
+    if (data.pwInSym) chars.push(...this.#symbols);
+    if (data.pwExcludeChar) {
+      const newPwExcludeChar = [...new Set(data.pwExcludeChar.split(''))]; //移除重複字元
+      chars = chars.filter(char => !newPwExcludeChar.includes(char));
+      if (chars.length === 0) return "no characters available"   //如果全部字元除外
+    }
+    let password = '';
+    for (let i = 0; i < Number(data.pwLength); i++){
+      password += chars[Math.floor(Math.random() * chars.length)]
+    }
+    return password;
+  }
+}
+
+module.exports = passwordGenerator;
